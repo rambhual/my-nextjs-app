2de5b1d355d160e0e88fef05c6677bd216e07abd
unknown <Rampritsahani@outlook.in>
2019-10-03 07:29:05 +0530

    first commit

---
+++ pages/products.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Segment } from "semantic-ui-react";
+const Products = () => {
+  return (
+    <Segment>
+      <h2>Welcome to product page!</h2>
+    </Segment>
+  );
+};
+
+export default Products;
